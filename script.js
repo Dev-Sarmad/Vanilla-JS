fd42d4f6be8493597362b85d2b3399ab79d233e9
Dev-Sarmad <sarmadh230@gmail.com>
2023-09-06 04:10:05 +0500

    event delegation

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const clearbtn = document.querySelector("#clear");
-const items = document.querySelectorAll(".items");
-const removeAll = () => {
-  items.forEach((item) => item.remove());
-};
-clearbtn.addEventListener("click", removeAll);
+// event deligation
+/*when we have to apply the event on the elements repeatidly or by looping over then we use 
+the event delegstion in which the parent element is selected for operation instead of all single element */
+
+// selecting the individual element
+const listItem = document.querySelectorAll('li');
+// listItem.forEach((item)=> {
+//   item.addEventListener('click',(e)=>{
+//     e.target.remove();
+//   })
+// })
+
+// selecting the parent element
+const list =  document.querySelector('ul');
+list.addEventListener('click',(e)=>{
+  if(e.target.tagName === 'LI'){
+    e.target.remove();
+    return;
+  }
+})
