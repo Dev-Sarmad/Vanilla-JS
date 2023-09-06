86ed48d4cd0127a6eaa806c4439ebfcd7529a279
Dev-Sarmad <sarmadh230@gmail.com>
2023-09-07 01:50:31 +0500

    Project's: create delete operation

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-// event deligation
-/*when we have to apply the event on the elements repeatidly or by looping over then we use 
-the event delegstion in which the parent element is selected for operation instead of all single element */
-
-// selecting the individual element
-const listItem = document.querySelectorAll('li');
-// listItem.forEach((item)=> {
-//   item.addEventListener('click',(e)=>{
-//     e.target.remove();
-//   })
-// })
-
-// selecting the parent element
-const list =  document.querySelector('ul');
-list.addEventListener('click',(e)=>{
-  if(e.target.tagName === 'LI'){
-    e.target.remove();
+const form = document.querySelector("#item-form");
+const formInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
+const clearButton  = document.getElementById('clear')
+const addItem = (e) => {
+  e.preventDefault();
+  // validation of user input
+  const input = formInput.value;
+  if (input === "") {
+    alert("Please enter a valid input");
     return;
+  } else {
+    console.log(input);
   }
-})
+  // creating new item
+  const item = document.createElement("li");
+  item.innerHTML = input;
+  const button = createButton("remove-item btn-link text-red");
+  item.appendChild(button);
+  itemList.appendChild(item);
+  // for to reset the input
+  formInput.value = "";
+};
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+const  removeItem = (e) => {
+  if(e.target.parentElement.classList.contains('remove-item')){
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+const clearItems = ()=>{
+  itemList.remove();
+}
+
+form.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearButton.addEventListener("click",clearItems);
